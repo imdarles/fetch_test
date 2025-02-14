2064bd4c5ba438ae79ce6fb9fdf5e001a23ed402
imdarles <dariaasb8@gmail.com>
2025-02-14 12:21:14 +0300

    Update code.js

--- code.js
+++ code.js
@@ -2,12 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const apiKey = 'ffedd3150ec0466d8fb82414251402';
 
   const fetchData = (cityName) => {
-     try {
-      const response = await fetch (`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=&{cityName}&aqi=no&lang=ru')
-      console.log(response);
-    } catch (error) {
-      console.log(error)
-    }
+    fetch (`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=&{cityName}&aqi=no&lang=ru')
+     .then(response => {
+       consile.log(response)
+     })
+    .catch(error => {
+      console.log('error', error);
+     })
   }
 
   fetchData('Yaroslavl');
