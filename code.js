6784f21f6425157366f7777d5232c8b031204810
imdarles <dariaasb8@gmail.com>
2025-02-14 11:48:40 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM fully loaded and parsed");
+  const apiKey = 'ffedd3150ec0466d8fb82414251402';
+
+  try {
+    const response = await fetch ('https://api.weatherapi.com/v1/current.json?key=ffedd3150ec0466d8fb82414251402&q=London&aqi=no')
+    console.log(response);
+  } catch (error) {
+    console.log(error)
+  }
 });
