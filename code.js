7ec47b17424c560f7b74575478d637647ecea62e
imdarles <dariaasb8@gmail.com>
2025-02-14 14:12:37 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const apiKey = 'ffedd3150ec0466d8fb82414251402';
 
-  const fetchData = async (cityName) => {
+  const fetchData = (cityName) => {
     fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no&lang=ru`);
-       .then(response => response.json())
-       .then(data => {
-         console.log(data)
+       .then(response => {
+         console.log(response)
        })
        .catch(error => {
          console.log('error', error);
